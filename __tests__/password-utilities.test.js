9508b531b97a0e39367e0871e9f2d454924ffabb
Yodirna <ridoy014@gmail.com>
2025-04-09 10:15:13 +0200

    add jest in package.json

---
+++ __tests__/password-utilities.test.js
@@ -0,0 +1,29 @@
+// password-utilities.test.js
+
+const { generateStrongPassword, isStrongPassword } = require("../index");
+
+describe("isStrongPassword", () => {
+  test("returns false for a weak password like 'Hello World'", () => {
+    expect(isStrongPassword("Hello World")).toBe(false);
+  });
+
+  test("returns true for a known strong password like 'pC%mD8TpCKn2'", () => {
+    expect(isStrongPassword("pC%mD8TpCKn2")).toBe(true);
+  });
+});
+
+describe("generateStrongPassword", () => {
+  test("generates a password of correct length", () => {
+    const length = 12;
+    const pw = generateStrongPassword(length);
+    expect(pw.length).toBe(length);
+  });
+
+  test("generated password meets strong criteria sometimes (not guaranteed yet)", () => {
+    const pw = generateStrongPassword(12);
+    // We won't expect it always to be strong at this point,
+    // just test that we get a string of length 12
+    expect(pw.length).toBe(12);
+  });
+});
+
